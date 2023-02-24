147de39fbe36c91424c770fe5f3a3395c55e036a
Vlad Belikov <v.belikov@aisnovations.com>
2023-02-24 16:33:51 +0300

    fix up migrations

--- ormconfig.ts
+++ ormconfig.ts
@@ -5,5 +5,5 @@ import { Config } from '@core/config';
 
 export default new DataSource({
   ...Config.get.typeORMOptions,
-  migrations: [join(__dirname, 'migrations/*-Migration{.ts,.js}')],
+  migrations: [join(__dirname, 'migrations', '*-Migration{.ts,.js}')],
 });
