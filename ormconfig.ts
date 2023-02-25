23abf422af44c89ccc7c6794e3d1d6917219526e
Vlad Belikov <v.belikov@aisnovations.com>
2023-02-25 11:18:28 +0300

    fix up migrations

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,9 +1,30 @@
+import { existsSync, mkdirSync, readdirSync } from 'fs';
 import { join } from 'path';
 import { DataSource } from 'typeorm';
 
 import { Config } from '@core/config';
 
+const folder = join(__dirname, 'migrations');
+
+if (!existsSync(folder)) {
+  mkdirSync(folder);
+}
+
+const migrations = readdirSync(folder).reduce(
+  (acc: FunctionConstructor[], migration: string) => {
+    acc.push(
+      ...Object.values(
+        // eslint-disable-next-line @typescript-eslint/no-var-requires
+        require(join(folder, migration)) as FunctionConstructor[],
+      ),
+    );
+
+    return acc;
+  },
+  [],
+);
+
 export default new DataSource({
   ...Config.get.typeORMOptions,
-  migrations: [join(__dirname, 'migrations', '*-Migration{.ts,.js}')],
+  migrations,
 });
