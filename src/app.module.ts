cdf6ca47a63764335588c59a9b3ba1fe6fd87b81
miloshevskayaa <miiloosheevskaayyaa@mail.ru>
2023-03-20 13:25:52 +0300

    modified app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 
 import { SharedModule } from './shared/shared.module';
-// import { APP_MODULES } from './modules';
+import { APP_MODULES } from './modules';
 
 @Module({
-  imports: [SharedModule.share()],
+  imports: [SharedModule.share(), ...APP_MODULES],
   controllers: [],
   providers: [],
 })
