5da0c6445392f6e56c70689f622da09d51c24270
miloshevskayaa <miiloosheevskaayyaa@mail.ru>
2023-03-26 16:02:51 +0300

    update validate method

--- src/modules/auth/models/jwt.strategy.ts
+++ src/modules/auth/models/jwt.strategy.ts
@@ -1,19 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-import { InjectRepository } from '@nestjs/typeorm';
 
 import { Config } from '@core/config';
-import { UserParent } from '@entities/users';
 import { UserService } from '@shared/user';
 
 import { ExtractJwt, Strategy } from 'passport-jwt';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(
-    @InjectRepository(UserParent)
-    private readonly _userService: UserService,
-  ) {
+  constructor(private readonly _userService: UserService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
@@ -22,7 +17,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: any) {
-    const user: any = await this._userService._getUserById(payload.sub);
+    const user = await this._userService._getUserById(payload.sub);
 
     return {
       user,
